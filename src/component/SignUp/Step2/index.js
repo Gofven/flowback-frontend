984d9137619c3f192843458e65c9ff067a4c7c43
Kattenelvis <officialkattenelvis@gmail.com>
2021-12-28 14:56:49 +0100

    when registred account user now get on login page

--- src/component/SignUp/Step2/index.js
+++ src/component/SignUp/Step2/index.js
@@ -85,7 +85,7 @@ export default function Step2({ stepNumber, totalStep, OnPrevious, OnNext, mainS
     console.log("le response", response)
     if (status === "success") {
       //history.push("/");
-      window.location.href = "/"
+      window.location.reload(false);
     } else {
       setError({ ...error, ...data[0] });
     }
