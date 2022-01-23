d99fd405dee493b9eeec24fd137f6e15e50dfada
Kattenelvis <officialkattenelvis@gmail.com>
2022-01-23 13:46:00 +0100

    mail function facelift

--- src/component/EmailCard/index.js
+++ src/component/EmailCard/index.js
@@ -3,11 +3,14 @@ import { Textbox } from "../../component/common/Textbox";
 import { Textarea } from "../../component/common/Textarea";
 import { inputKeyValue } from '../../utils/common';
 import { postRequest } from "../../utils/API";
+import Loader from "../../component/common/Loader";
 
 export default function SendEmail({groupId})
 {
     const [state, setState] = useState({subject:"", message:""});
     const {subject, message} = state;
+    const [status, setStatus] = useState({text:"", color:"black"});
+    const [loading, setLoading] = useState(false);
 
     const handleOnChange = (e) => {
         console.log(state)
@@ -16,12 +19,19 @@ export default function SendEmail({groupId})
 
       const handleSendMail = () => {
         // console.log(groupId)
+        setLoading(true)
         var data = new FormData();
         data.append("subject", state.subject);
         data.append("message", state.message);
 
         postRequest(`api/v1/user_group/${groupId}/mail_all_group_members`, data).then(response => {
-            console.log(response);
+            setLoading(false)
+            if (response === "")
+            {
+                setStatus({text:"Successfully sent mail", color:"green"});
+            }
+        }).catch(() => {
+            setStatus({text:"Something went wrong", color:"red"});
         })
       }
 
@@ -29,35 +39,38 @@ export default function SendEmail({groupId})
     <div className="document-card card-rounded mb-4">
         <div className="card-header flex-header tab-header">
             <h4 className="card-title">Send Email</h4>
+            <h4 style={{"color":status.color}}>{status.text}</h4>
         </div>
-        <div className="card-body">
-        <form className="form login_form" id="loginForm">
-            <div className="form-group">
-            <h5>Title</h5>
-            <Textbox
-                name="subject"
-                value={subject}
-                onChange={handleOnChange}
-                required
-                />
+        <Loader loading={loading}>
+            <div className="card-body">
+            <form className="form login_form" id="loginForm">
+                <div className="form-group">
+                <h5>Title</h5>
+                <Textbox
+                    name="subject"
+                    value={subject}
+                    onChange={handleOnChange}
+                    required
+                    />
+                </div>
+                <div className="form-group" style={{"margin-top":"3%"}}>
+                <h5>Email</h5>
+                <Textarea
+                    name="message"
+                    value={message}
+                    onChange={handleOnChange}
+                    required
+                    />
+                </div>
+                <button
+                    type="button"
+                    className="btn btn-primary btn-hover"
+                    //disabled={!formVaxlid}
+                    onClick={handleSendMail}>
+                    Send
+                </button>
+            </form>
             </div>
-            <div className="form-group">
-            <h5>Email</h5>
-            <Textarea
-                name="message"
-                value={message}
-                onChange={handleOnChange}
-                required
-                />
-            </div>
-            <button
-                type="button"
-                className="btn btn-primary btn-hover"
-                //disabled={!formVaxlid}
-                onClick={handleSendMail}>
-                Send
-            </button>
-        </form>
-        </div>
+        </Loader>
         </div>)
 }
