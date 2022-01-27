597af64a71c2be47255707791d51c89ed5426cd9
Kattenelvis <officialkattenelvis@gmail.com>
2022-01-27 14:38:52 +0100

    expanding description

--- src/page/Polls/CounterProposals/SortCounterProposal/index.js
+++ src/page/Polls/CounterProposals/SortCounterProposal/index.js
@@ -28,7 +28,7 @@ import { formatDate } from '../../../../utils/common';
 import Profile from '../../../../component/User/Profile';
 import LinesEllipsis from 'react-lines-ellipsis';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { Condorcet, TrafficLight } from './VoteButtons';
 
 const div = styled.div`
@@ -41,6 +41,7 @@ const Title = styled.h5`
   margin-bottom: 0;
   text-align:center;
   font-size:2rem;
+  font-weight:680;
 `;
 const TaskList = styled.div`
   padding: 8px 16px;
@@ -93,6 +94,7 @@ function Column(props) {
 }
 
 function ProposalBox(props) {
+    const [expandedDescription, setExpandedDescription] = useState(false)
     const counterProposal = props.task.content;
     counterProposal.title = counterProposal?.proposal.split("~")[0];
     counterProposal.description = counterProposal?.proposal.split("~")[1];
@@ -124,28 +126,37 @@ function ProposalBox(props) {
                         text={counterProposal?.description}
                         ellipsis="..."
                         trimRight
-                        maxLine="3"
+                        maxLine={`${expandedDescription ? "1000" : "3"}`}
                         basedOn='letters' />
+
+                    {counterProposal?.description !== "No description" &&
+                        <FontAwesomeIcon className={`fa expand-description-circle ${expandedDescription ? "clicked" : null}`}
+                            icon={faArrowCircleDown}
+                            color=''
+                            size='lg'
+                            onClick={() => setExpandedDescription(!expandedDescription)} />}
+
                 </div>
-
-
-                {/* The backend only supports one textfield for a proposal so putting "~" between the title and description is a workaround */}
             </div>
 
-            {props.votingType === "traffic" && <TrafficLight {...props} iconSize={"fa-3x"} />}
-            {props.votingType === "condorcet" && <Condorcet {...props} iconSize={"fa-3x"} />}
+            <div className="proposal-buttons-and-user">
+                {/* The backend only supports one textfield for a proposal so putting "~" between the title and description is a workaround */}
 
-            {counterProposal && counterProposal.user &&
-                <div className="media post-meida">
-                    {/* <Image src={counterProposal.user.image} className="post-user-img" errImg={'/img/no-photo.jpg'} /> */}
-                    <div className="media-body">
-                        <a className="user-name user-name-proposal">
-                            <Profile className='inline-block' id={counterProposal.user.id}>{counterProposal.user.first_name} {counterProposal.user.last_name} </Profile>
-                        </a>
-                        <div className="post-time">{counterProposal && formatDate(counterProposal.created_at, 'DD/MM/YYYY kk:mm')}</div>
+                {counterProposal && counterProposal.user &&
+                    <div className="media post-meida">
+                        {/* <Image src={counterProposal.user.image} className="post-user-img" errImg={'/img/no-photo.jpg'} /> */}
+                        <div className="media-body proposal-bottom">
+                            <a className="user-name user-name-proposal">
+                                <Profile className='inline-block' id={counterProposal.user.id}>{counterProposal.user.first_name} {counterProposal.user.last_name} </Profile>
+                            </a>
+                            <div className="post-time">{counterProposal && formatDate(counterProposal.created_at, 'DD/MM/YYYY kk:mm')}</div>
+                        </div>
                     </div>
-                </div>
-            }
+                }
+                {/* </div> */}
+                {props.votingType === "traffic" && <TrafficLight {...props} iconSize={"fa-4x"} />}
+                {props.votingType === "condorcet" && <Condorcet {...props} iconSize={"fa-4x"} />}
+            </div>
         </div>
     </div>
 }
