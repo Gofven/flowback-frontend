c06090acc6a721ab730411fa26eafeafc1494911
Cactus <cactusdevcode@gmail.com>
2022-02-27 23:01:23 +0100

    Add group statistics

--- src/component/GroupStatsCard/index.js
+++ src/component/GroupStatsCard/index.js
@@ -1,112 +1,72 @@
-import React, { useState, useEffect } from "react";
-import { Textbox } from "../../component/common/Textbox";
-import { Textarea } from "../../component/common/Textarea";
-import {FormatComments, inputKeyValue} from '../../utils/common';
-import { postRequest } from "../../utils/API";
+import React, {useState, useEffect} from "react";
+import {postRequest} from "../../utils/API";
 import Loader from "../../component/common/Loader";
 
-export default function GroupStatsCard({ groupId, userType }) {
-    const [state, setState] = useState({ subject: "", message: "" });
-    const { subject, message } = state;
-    const [status, setStatus] = useState({ text: "", color: "black" });
-    const [loading, setLoading] = useState(false);
+export default function GroupStatsCard({groupId, userType}) {
+    const [pollCount, setPollCount] = useState(0);
+    const [documentCount, setDocumentCount] = useState(0);
+    const [memberCount, setMemberCount] = useState(0);
 
-    const handleOnChange = (e) => {
-        console.log(state)
-        setState({ ...state, ...inputKeyValue(e) });
-    };
-
-    const handleSendMail = () => {
-        // console.log(groupId)
-        setLoading(true)
-        var data = new FormData();
-        data.append("subject", state.subject);
-        data.append("message", state.message);
-
-        postRequest(`api/v1/user_group/${groupId}/mail_all_group_members`, data).then(response => {
-            setLoading(false)
-            if (response === "") {
-                setStatus({ text: "Successfully sent mail", color: "green" });
-            }
-        }).catch(() => {
-            setStatus({ text: "Something went wrong", color: "red" });
-        })
-    }
+    const [loading, setLoading] = useState(true);
+    const [pollsLoading, setPollsLoading] = useState(true);
+    const [documentLoading, setDocumentLoading] = useState(true);
+    const [memberLoading, setMemberLoading] = useState(true);
 
     function getAllGroupDocs() {
-        postRequest("api/v1/user_group/get_all_group_docs", { group: groupId }).then(
+        postRequest("api/v1/user_group/get_all_group_docs", {group: groupId}).then(
             (response) => {
-                console.log('api/v1/user_group/get_all_group_docs', response);
-                /*const { status, data } = response;
-                if (status === "success") {
-                    setDocuments(data);
-                } else {
-                    // setError();
+                if (response.data?.length) {
+                    setDocumentCount(response.data.length);
                 }
-                setLoading(false);*/
             }
-        );
+        ).finally(() => setDocumentLoading(false));
     }
 
     function getPolls(pollType) {
-        let data = {
+        // The payload doesn't matter always gives all polls from all joined groups
+        /*let data = {
             first_page: true,
             last_poll_created_at: '',
             page: 1,
-            page_size: 1000,
+            page_size: 100000000000000,
             poll_type: pollType,
-            group: groupId
-        }
-        postRequest("api/v1/group_poll/home_all_poll_list", {data}).then(
+            group: 1
+        }*/
+        postRequest("api/v1/group_poll/home_all_poll_list", {}).then(
             (response) => {
-                console.log('response', response);
-                if (response) {
-                    console.log("api/v1/group_poll/home_all_poll_list",response)
-                    /*const { status, data: res } = response;
-                    if (res != undefined) {
-                        const data = res.data;
-                        if (status == "success") {
-                            if (first_page) {
-                                setLastPollCreatedDate(res.last_poll_created_at);
-                            }
-                            console.log('poll data', data.data)
-                            if (data) {
-                                data.forEach((poll) => {
-                                    poll.comments_details.comments = FormatComments(poll.comments_details.comments);
-                                })
-                            }
-                            setPolls([...polls, ...data]);
-                            setPage(page + 1);
-                            setTotalPage(res.total_page);
-                        }
-                    }*/
+                if (response.data?.count) {
+                    setPollCount(response.data.count);
                 }
             }
-        );
+        ).finally(() => setPollsLoading(false));
     }
-    
+
     function getGroupParticipants() {
-        //setLoading(true)
-        postRequest("api/v1/user_group/group_participants", { id: groupId }).then(
+        postRequest("api/v1/user_group/group_participants", {id: groupId}).then(
             (response) => {
                 if (response) {
-                    console.log("api/v1/user_group/group_participants",response);
-                    //const { status, data } = response;
-                    //setMembers(data.participant);
-                    //setTotalMemebers(data.total_participant);
+                    if (response.data?.total_participant) {
+                        setMemberCount(response.data.total_participant);
+                    }
 
                 }
-                //setLoading(false)
             }
-        );
+        ).finally(() => setMemberLoading(false));
     }
-    
+
     useEffect(() => {
         getAllGroupDocs();
-        getPolls('condorcet');
+        getPolls();
         getGroupParticipants();
     }, [])
-    
+
+    useEffect(() => {
+        if (!pollsLoading && !documentLoading && !memberLoading) {
+            setLoading(false)
+        }
+    }, [pollsLoading, documentLoading, memberLoading]);
+
+
     return (
         userType === "Owner" ?
             <div className="document-card card-rounded mb-4">
@@ -114,8 +74,10 @@ export default function GroupStatsCard({ groupId, userType }) {
                     <h4 className="card-title">Group statistics</h4>
                 </div>
                 <Loader loading={loading}>
-                    <div className="card-body">
-                        <StatsNumberCard label="Members" number={1305}/>
+                    <div className="card-body d-flex flex-row justify-content-center">
+                        <StatsNumberCard label="Members" number={memberCount}/>
+                        {/*<StatsNumberCard label="Polls" number={pollCount}/>*/}
+                        <StatsNumberCard label="Documents" number={documentCount}/>
                     </div>
                 </Loader>
             </div> : <div className="document-card card-rounded mb-4">
@@ -127,10 +89,10 @@ export default function GroupStatsCard({ groupId, userType }) {
             </div>)
 }
 
-function StatsNumberCard({label = "label",number=0}) {
-    return <div className="card">
+function StatsNumberCard({label = "label", number = 0}) {
+    return <div className="card m-2" style={{width: "8rem"}}>
         <div className="card-body">
-            <p className="mb-1 text-secondary">{label}</p>
+            <p className="mb-1 text-secondary ">{label}</p>
             <h2 className="fw-bold">{number}</h2>
         </div>
     </div>;
