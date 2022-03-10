31806787361a7724215ba2b387b311a442d42a6d
Kattenelvis <officialkattenelvis@gmail.com>
2022-03-10 14:38:25 +0100

    s

--- src/page/Polls/CounterProposals/SortCounterProposal/index.js
+++ src/page/Polls/CounterProposals/SortCounterProposal/index.js
@@ -285,8 +285,7 @@ function SortCounterProposal(props) {
                 signData({
                     positive: positive_proposal_indexes_2,
                     negative: negative_proposal_indexes_2
-                },
-                    userId, props.counterProposals, props.proposalIndexes).then(signedData => {
+                },userId, props.counterProposals, props.proposalIndexes).then(signedData => {
                         const encryptedSignedData = encryptWithPublicKey(signedData, publicKey);
                         const data = {
                             positive: positive_proposal_indexes_2,
