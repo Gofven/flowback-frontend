6c27a2247dd762777dee4a3b6682dfb5fd6f8f9c
Kattenelvis <officialkattenelvis@gmail.com>
2022-03-23 20:58:20 +0100

    chat

--- src/component/Chat/GroupChannels.js
+++ src/component/Chat/GroupChannels.js
@@ -1,7 +1,41 @@
+import { useEffect } from "react";
+
 export default function GroupChannels() {
+
+  let socket = new WebSocket("wss://demo.flowback.org/ws/chat/test");
+  
+  console.log(socket)
+
+  socket.onopen = function(event) {
+    alert("[open] Connection established");
+    alert("Sending to server");
+    socket.send("My name is John");
+  };
+  useEffect(() =>{
+    
+    // socket.onmessage = function(event) {
+    //   alert(`[message] Data received from server: ${event.data}`);
+    // };
+    
+    // socket.onclose = function(event) {
+    //   if (event.wasClean) {
+    //     alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    //   } else {
+    //     // e.g. server process killed or network down
+    //     // event.code is usually 1006 in this case
+    //     alert('[close] Connection died');
+    //   }
+    // };
+    
+    // socket.onerror = function(error) {
+    //   alert(`[error] ${error.message}`);
+    // };
+  },[])
+
+
+
   return (
     <div>
-      {/* <div>GOOD MORNING VIETNAM</div> */}
       <div>
         <div>ny grupp</div>
 
