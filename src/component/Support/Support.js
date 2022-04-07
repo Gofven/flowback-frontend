ec5954d65261baf75aa3579c3ee347ed3356bd3a
Kattenelvis <officialkattenelvis@gmail.com>
2022-04-08 00:56:43 +0200

    support

--- src/component/Support/Support.js
+++ src/component/Support/Support.js
@@ -15,6 +15,7 @@ export default function Support() {
                 <div>{window.t("Support between 12:00 and 17:00 CET")}</div>
                 <div>{window.t("Number: +46737482562")}</div>
                 <div>{window.t("Mail: flowbacktask@gmail.com")}</div>
+                <div>{window.t("For questions about Metamask, contact the Metamask team.")}</div>
             </Modal.Body>
             <Modal.Footer><button className="btn btn-danger" onClick={handleClose}>{window.t("Close")}</button></Modal.Footer>
         </Modal>
