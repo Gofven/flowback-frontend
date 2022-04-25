45428f06cff5abba789ac4042f87f4a819a9f1e5
Kattenelvis <officialkattenelvis@gmail.com>
2022-04-25 14:31:45 +0200

    predict

--- src/page/Prediction/Prediction.js
+++ src/page/Prediction/Prediction.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 import Loader from "../../component/common/Loader/Loader";
 
 export default function Prediction({ prediction }) {
-    const [score, setScore] = useState(50)
+    const defaultVote = 0
+    const [score, setScore] = useState(defaultVote)
+    const [averageScore, setAverageScore] = useState(defaultVote)
     const [loading, setLoading] = useState(false)
     const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false)
 
@@ -14,20 +16,21 @@ export default function Prediction({ prediction }) {
         postRequest("api/v1/prediction/vote/create", { post: prediction.id, score: score }).then(res => {
             setLoading(false)
             setHasUnsavedChanges(false)
+            voteGet()
         })
     }
 
     const voteDelete = (e) => {
         e.preventDefault()
         postRequest("api/v1/prediction/vote/delete", { post: prediction.id }).then(res => {
-            setScore(50)
+            setScore(defaultVote)
             setHasUnsavedChanges(false)
         })
     }
 
     const voteGet = (e) => {
         getRequest("api/v1/prediction/vote/" + prediction.id).then(res => {
-            setScore(res.score || 50)
+            setAverageScore(res.score || defaultVote)
             setHasUnsavedChanges(false)
         })
     }
@@ -48,10 +51,14 @@ export default function Prediction({ prediction }) {
             <div className="mt-2 mb-3">{prediction.description}</div>
             <form>
                 <div className="w-100">
-                    <div className="d-flex">0<input className="w-100" type="range" id="prediction-vote-slider" max="100" min="0" value={score} onChange={scoreChange}></input>100</div>
-                    <div className="text-center" style={{ "color": hasUnsavedChanges ? "#994422" : "black" }}>{score}%</div>
+                    <div className="d-flex ">
+                        0% <input className="w-100" type="range" id="prediction-vote-slider" max="5" min="0" value={score} onChange={scoreChange}></input> 100%
+                        {/* <input className="w-100 bg-orange-400" type="range" id="prediction-vote-slider" max="5" min="0" value={averageScore} ></input> */}
+                    </div>
+                    <div className="text-center" style={{ "color": hasUnsavedChanges ? "#994422" : "black" }}>{score * 20}%</div>
                 </div>
                 {hasUnsavedChanges && <div>You have unsaved changes</div>}
+                <div>Average score: {averageScore * 20}%</div>
                 <div className="d-flex mt-3">
                     <button type="submit" onClick={voteCreate} className="btn btn-primary">Vote</button>
                     <button type="submit" className="btn btn-secondary ms-2" onClick={voteDelete}>Unvote</button>
